6920c967b0876eaf1892ca3f6338a2a0c62b05f1
Marsel <92223429+web-proger04@users.noreply.github.com>
2022-09-26 22:57:47 +0500

    добавил страницу где все колледжи и футер

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../styles/navbar.module.scss'
+import Link from "next/link";
+
+
+function Navbar() {
+    return (
+        <footer className="footer">
+            Все права защищены 2022
+        </footer>
+    );
+}
+
+export default Navbar;
